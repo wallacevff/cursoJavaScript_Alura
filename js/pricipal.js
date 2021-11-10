376bbc2b0efc27ac6fdc9937b95f071d01099513
Wallace Vidal de Figueiredo Fortuna <wallacevff@hotmail.com>
2021-11-10 03:52:32 -0300

    Primeiro Commit

---
+++ js/pricipal.js
@@ -0,0 +1,13 @@
+var titulo = document.querySelector("#titulo");
+/*console.log(titulo);
+console.log(titulo.textContent);
+
+*/
+titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdpeso = paciente.querySelector(".info-peso");
+var peso = tdpeso.textContent;
+
+var tdaltura = paciente.querySelector(".info-altura");
+var altura = tdaltura.textContent;
