f657e0dc0de9a7faef669d19bad2b2f980cd4f37
Wallace Vidal de Figueiredo Fortuna <wallacevff@hotmail.com>
2021-11-10 22:01:46 -0300

    Calculo IMC todos os pacientes

--- js/pricipal.js
+++ js/pricipal.js
@@ -5,12 +5,42 @@ console.log(titulo.textContent);
 */
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdpeso = paciente.querySelector(".info-peso");
-var peso = tdpeso.textContent;
+var pacientes = document.querySelectorAll(".paciente");
+pacientes.forEach(function (paciente, i) {
+    var tdpeso = paciente.querySelector(".info-peso");
+    var peso = tdpeso.textContent;
 
-var tdaltura = paciente.querySelector(".info-altura");
-var altura = tdaltura.textContent;
+    var tdaltura = paciente.querySelector(".info-altura");
+    var altura = tdaltura.textContent;
 
-var imc = peso / (altura * altura);
-console.log(imc);
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    var mensagem = "";
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso Inválido");
+        pesoEhValido = false;
+        mensagem += "Peso inválido ";
+    }
+
+    if (altura <= 0 || altura >= 3) {
+        console.log("Altura inválida");
+        alturaEhValida = false;
+        mensagem += "Altura inválida";
+    }
+
+    if (pesoEhValido && alturaEhValida) {
+        var imc = peso / (altura * altura);
+        imc = imc.toFixed(2);
+    }
+    else {
+        imc = mensagem;
+    }
+
+
+    var imcpaciente = paciente.querySelector(".info-imc");
+    imcpaciente.textContent = imc;
+});
+
+//console.log(imcpaciente);
